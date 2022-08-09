a316dbd40a24201cf22f8beb6b6ebd947e5e8d60
hokhk <hokhk@hok.com>
2022-08-09 18:18:36 +0800

    Add pyright and lau-language-server executable location to /home/ubuntu/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/ubuntu/.fzf/bin

--- lua/myCustom/plugin/lspconfig.lua
+++ lua/myCustom/plugin/lspconfig.lua
@@ -9,7 +9,9 @@ return function (use)
                 debounce_text_changes = 150,
             }
             local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-            require('lspconfig')['pyright'].setup{
+            vim.env.PATH=vim.env.PATH..":/home/ubuntu/.local/bin"
+            vim.env.PATH=vim.env.PATH..":/home/ubuntu/lua-language-server/bin"
+            require'lspconfig'.pyright.setup{
                 --              on_attach = on_attach,
                 flags = lsp_flags,
                 capabilities=capabilities
@@ -20,7 +22,8 @@ return function (use)
             }
             require'lspconfig'.sumneko_lua.setup {
                 settings = {
-                    Lua = {
+                    Lua = 
+                    {
                         runtime = {
                             -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
                             version = 'LuaJIT',
