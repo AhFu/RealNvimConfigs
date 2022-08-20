6388f8732d32d465f0f8deddc4de52567476a3e0
hokhk <hok.hk@hok.com>
2022-08-20 22:23:29 +0800

    add code to window.lua

--- lua/myCustom/script/window.lua
+++ lua/myCustom/script/window.lua
@@ -1,6 +1,31 @@
 return function()
     local char_key=0 local key=0
     local increment=3
+    local ns_id=nil
+    local mark_id=nil
+
+    function  ShowVirtualText ()
+        local ns_id = vim.api.nvim_create_namespace('demo')
+        vim.api.nvim_set_hl(0,"OnFocus",{fg="orange",bg="black",italic=true})
+     --   local bnr = vim.fn.bufnr('%')
+        local line_num =0
+        local col_num =0
+        local opts = {
+            virt_text = {{"OnFocus", "OnFocus"}},
+            virt_text_pos = 'right_align',
+            hl_mode='combine',
+        }
+        local mark_id= vim.api.nvim_buf_set_extmark(0, ns_id, line_num, col_num, opts)
+      --  print (ns_id.."insdie function1")
+       -- print(mark_id.."inside function2")
+        return ns_id, mark_id
+    end
+    function RemoveVirtualText(nsID,markID)
+        if ns_id~=nil and mark_id~=nil then
+            vim.api.nvim_buf_del_extmark(0,nsID,markID)
+        end
+    end
+
     function ResizeWindow(k)
         vim.b.increment=increment
         if k==10 then
@@ -22,11 +47,14 @@ return function()
         vim.cmd([[:exec "normal!" "\<c-w>" . b:key]])
     end
     function RefreshScreen()
-
         vim.cmd(":redraw!")
         vim.cmd([[:exec "normal!" "\<c-l>"]])
     end
+
+
     repeat
+        ns_id,mark_id=   ShowVirtualText()
+        RefreshScreen()
         char_key=vim.fn.getchar()
         key=vim.fn.nr2char(char_key)
         if char_key==8 or char_key==10 or char_key==11 or char_key==12 then
@@ -36,6 +64,8 @@ return function()
         if key=='h' or key=='j' or key=='k' or key=='l' then
             FocusWindow(key)
             vim.cmd([[:sleep 100ms]])
+            RemoveVirtualText(ns_id,mark_id)
+            ns_id,mark_id=   ShowVirtualText()
             RefreshScreen()
             vim.cmd(":echo @%")
         end
@@ -53,5 +83,6 @@ return function()
             char_key=120 -- type "x"to exit the program
         end
     until char_key==120 --type "x" to exit the program
+    RemoveVirtualText(ns_id,mark_id)
 end
 
